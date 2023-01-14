e7de79f831e6c7ab74ba68298ebf9338c6263938
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-14 22:51:17 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
+import javax.xml.transform.TransformerException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
-        for (int i = 0; i < 100; i++) {
-            System.out.print(i);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new ArrayList<>();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    list1.add(i);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    list2.add(i);
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    list3.add(i);
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println("\nFinish");
+        System.out.println(list1.size());
+        System.out.println(list2.size());
+        System.out.println(list3.size());
     }
 }
