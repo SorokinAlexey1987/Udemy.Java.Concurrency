042503cf27438afdc616b23b0d573c55461ea4b2
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-14 23:15:20 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import javax.xml.transform.TransformerException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static List<String> list;
+
     public static void main(String[] args) {
-        System.out.println("Start");
-        for (int i = 0; i < 100; i++) {
-            System.out.print(i);
-        }
-        System.out.println("\nFinish");
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                list = new ArrayList<>();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long before = System.currentTimeMillis();
+                try {
+                    thread1.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(list.size());
+                long after = System.currentTimeMillis();
+                System.out.println(after - before);
+            }
+        });
+        thread1.start();
+        thread2.start();
     }
 }
