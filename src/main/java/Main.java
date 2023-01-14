20f45c0da8c028d96feaf5b5b2c5e76966beb2c5
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-14 22:35:38 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
+import javax.xml.transform.TransformerException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
-        for (int i = 0; i < 100; i++) {
-            System.out.print(i);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int j = 0; j < 10; j++) {
+                        if (Thread.currentThread().isInterrupted()) {
+                            break;
+                        }
+                        System.out.print(j);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (i == 10) {
+                thread.interrupt();
+            }
+            System.out.print("M");
         }
         System.out.println("\nFinish");
     }
