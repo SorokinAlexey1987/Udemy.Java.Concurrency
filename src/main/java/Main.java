8f32e59bcd4146a0377d6364333c9ad7623186cb
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-14 15:42:44 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i);
+        }
+        System.out.println("\nFinish");
+    }
+}
