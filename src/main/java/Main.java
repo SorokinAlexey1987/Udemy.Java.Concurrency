f2a622dcb3ee9bdcc2e43950dc4f1928210a9cb2
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-16 00:03:39 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,10 +12,13 @@ public class Main {
         //withoutConcurrency();
 
         Counter counter = new Counter();
+        long before = System.currentTimeMillis();
+        int barrier = 100_000_000;
+
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i < 1000; i++) {
+                for(int i = 0; i < barrier; i++) {
                     counter.inc();
                 }
             }
@@ -23,20 +26,43 @@ public class Main {
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i < 1000; i++) {
+                for(int i = 0; i < barrier; i++) {
                     counter.dec();
+                }
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.inc2();
+                }
+            }
+        });
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.dec2();
                 }
             }
         });
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
+            thread4.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        long after = System.currentTimeMillis();
         System.out.println(counter.getValue());
+        System.out.println(counter.getValue2());
+        System.out.println(after - before);
     }
 
 
