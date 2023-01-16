b5c9c636738e72142e17d806234fa2c21c03026b
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-16 16:15:16 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,36 @@ public class Main {
     //private static final int HALF = SIZE / 2;
 
     public static void main(String[] args) {
+        Counter2 counter2 = new Counter2();
+        int barrier = 1_000_000;
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter2.inc();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter2.dec();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(counter2.getValue());
+
+        /*
         ATM atm = new ATM(1000);
 
         Thread thread1 = new Thread(new Runnable() {
@@ -33,6 +63,7 @@ public class Main {
         thread1.start();
         thread2.start();
         thread3.start();
+        */
 
         /*
         Counter counter = new Counter();
