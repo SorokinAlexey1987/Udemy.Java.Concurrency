2e4d8dfa1ef6b9698ab4febc21c3ede92f8bbf67
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-17 00:28:06 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,66 @@ public class Main {
     //private static final int HALF = SIZE / 2;
 
     public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Runnable task1 = new Runnable() {
+            @Override
+            public void run() {
+                long sum = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 2 == 0) {
+                        sum += i;
+                    }
+                }
+                System.out.println("Task1: " + sum);
+                countDownLatch.countDown();
+            }
+        };
+        Runnable task2 = new Runnable() {
+            @Override
+            public void run() {
+                long sum = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 7 == 0) {
+                        sum += i;
+                    }
+                }
+                System.out.println("Task2: " + sum);
+                countDownLatch.countDown();
+            }
+        };
+        Runnable task3 = new Runnable() {
+            @Override
+            public void run() {
+                List<Integer> numbers = new ArrayList<>();
+                for (int i = 0; i < 1000; i++) {
+                    numbers.add((int) (Math.random() * 1000));
+                }
+                int count = 0;
+                for (int number : numbers) {
+                    if (number % 2 == 0) {
+                        count++;
+                    }
+                }
+                System.out.println("Task3: " + count);
+                countDownLatch.countDown();
+            }
+        };
+        long before = System.currentTimeMillis();
+        executorService.execute(task1);
+        executorService.execute(task2);
+        executorService.execute(task3);
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+
+
+        /*
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         CountDownLatch countDownLatch = new CountDownLatch(10);
         for (int i = 0; i < 10; i++) {
@@ -36,8 +96,7 @@ public class Main {
             throw new RuntimeException(e);
         }
         System.out.println("All threads are terminated");
-
-
+        */
 
         /*
         Counter2 counter2 = new Counter2();
