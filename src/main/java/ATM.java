4df9910af87a0d00a91abd5cec01f22c507f4be4
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-16 15:35:27 +0300

    Initial commit

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,23 +1,26 @@
 public class ATM {
     private int amount;
+    private final Object monitor = new Object();
 
     public ATM(int amount) {
         this.amount = amount;
     }
 
     public void withdraw(String name, int amount) {
-        System.out.println(name + " went to the ATM");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (amount <= this.amount) {
-            this.amount -= amount;
-            System.out.println(name + " withdrew " + amount);
-            System.out.println("Left: " + this.amount);
-        } else {
-            System.out.println("There is not enought money in the account for " + name);
+        synchronized (monitor) {
+            System.out.println(name + " went to the ATM");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (amount <= this.amount) {
+                this.amount -= amount;
+                System.out.println(name + " withdrew " + amount);
+                System.out.println("Left: " + this.amount);
+            } else {
+                System.out.println("There is not enought money in the account for " + name);
+            }
         }
     }
 }
