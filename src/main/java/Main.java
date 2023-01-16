16d8fe23c661411e92ef2860f6438dd85fbe8670
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-17 00:04:17 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,239 @@
+import javax.xml.transform.TransformerException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+    //private static final int SIZE = 50_000_000;
+    //private static final int HALF = SIZE / 2;
+
     public static void main(String[] args) {
-        System.out.println("Start");
-        for (int i = 0; i < 100; i++) {
-            System.out.print(i);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Start - " + index);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Finish - " + index);
+                    countDownLatch.countDown();
+                }
+            });
         }
-        System.out.println("\nFinish");
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("All threads are terminated");
+
+
+
+        /*
+        Counter2 counter2 = new Counter2();
+        int barrier = 1_000_000;
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter2.inc();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter2.dec();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(counter2.getValue());
+        */
+
+        /*
+        ATM atm = new ATM(1000);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("Max", 300);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("John", 500);
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("Nick", 400);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        */
+
+        /*
+        Counter counter = new Counter();
+        long before = System.currentTimeMillis();
+        int barrier = 100_000_000;
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.inc();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.dec();
+                }
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.inc2();
+                }
+            }
+        });
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < barrier; i++) {
+                    counter.dec2();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(counter.getValue());
+        System.out.println(counter.getValue2());
+        System.out.println(after - before);
+    */
+
+    /*
+    startTimer();
+    withConcurrency();
+    withoutConcurrency();
+
+    private static void startTimer() {
+        Thread timer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int seconds = 0;
+                try {
+                    while (true) {
+                        System.out.println(seconds++);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        timer.setDaemon(true);
+        timer.start();
+    }
+
+    private static void withoutConcurrency() {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
+        }
+        long before = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            float f = (float) i;
+            array[i] = (float) (array[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("TimeWithoutConcurrency = " + (after - before));
+    }
+
+    private static void withConcurrency() {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
+        }
+        long before = System.currentTimeMillis();
+        float[] firstHalf = new float[HALF];
+        float[] secondHalf = new float[HALF];
+        System.arraycopy(array, 0, firstHalf, 0, HALF);
+        System.arraycopy(array, HALF, secondHalf, 0, HALF);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) i;
+                    firstHalf[i] = (float) (firstHalf[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) (i + HALF);
+                    secondHalf[i] = (float) (secondHalf[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.arraycopy(firstHalf, 0, array, 0, HALF);
+        System.arraycopy(secondHalf, 0, array, HALF, HALF);
+        long after = System.currentTimeMillis();
+        System.out.println("TimeWithConcurrency = " + (after - before));
+    }
+    */
     }
 }
