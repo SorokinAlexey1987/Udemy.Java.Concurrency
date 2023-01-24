f6cfff14912730d47cd86dba9dd1a9c5b19f3d2d
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-24 22:49:05 +0300

    Initial commit

--- src/main/java/BlockingQueue.java
+++ src/main/java/BlockingQueue.java
@@ -2,13 +2,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BlockingQueue {
-    Queue<Runnable> queue = new LinkedList<>();
+
+    private final Queue<Runnable> queue = new LinkedList<>();
+    private final Object monitor = new Object();
 
     public void add(Runnable task) {
-        queue.add(task);
+        synchronized (monitor) {
+            queue.add(task);
+            monitor.notify();
+        }
     }
 
     public Runnable take() {
-        return queue.poll();
+        synchronized (monitor) {
+            while (queue.isEmpty()) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return queue.poll();
+        }
     }
 }
