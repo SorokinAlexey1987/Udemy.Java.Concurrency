efa7128ead4bb1f253beab303a2986d31991818b
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-24 22:33:51 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,32 @@ public class Main {
 
     public static void main(String[] args) {
         BlockingQueue blockingQueue = new BlockingQueue();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    Runnable task = blockingQueue.take();
+                    if (task != null) {
+                        new Thread(task).start();
+                    }
+                }
+            }
+        }).start();
 
-
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            blockingQueue.add(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("---" + index);
+                }
+            });
+        }
 
 
         /*
