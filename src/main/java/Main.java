eadd68179ed8bce2a05757b22076ee62526f2bd9
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-26 17:00:35 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,7 +31,29 @@ public class Main {
     public static void main(String[] args) {
 
         //Task_14
-
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+        for (int i = 0; i < 10; i++) {
+            new Thread (new Runnable() {
+                @Override
+                public void run() {
+                    long millis = (long) (Math.random() * 5000 + 1000);
+                    String name = Thread.currentThread().getName();
+                    System.out.println(name + ": Data is being prepared");
+                    try {
+                        Thread.sleep(millis);
+                        System.out.println(name + ": Data is ready");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        cyclicBarrier.await();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(name + ": Continue work");
+                }
+            }).start();
+        }
 
 
 
