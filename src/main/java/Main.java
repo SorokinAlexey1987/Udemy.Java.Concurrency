4538c22d51531ce2e524e7da67f2120f44d2bc0d
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-26 16:48:57 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,381 @@
 import javax.xml.transform.TransformerException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
-    //private static final int SIZE = 50_000_000;
-    //private static final int HALF = SIZE / 2;
+    /*
+    private static final int SIZE = 50_000_000;
+    private static final int HALF = SIZE / 2;
+    private static final String A = "A";
+    private static final String B = "B";
+    private static final String C = "C";
+    private static final Object MONITOR = new Object();
+    private static String nextLetter = A;
+    */
+    private static void workWithFileSystem() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " started working with file system.");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(name + " finished working with file system.");
+    }
+
 
     public static void main(String[] args) {
+
+        //Task_14
+
+
+
+
+        //Task_13
+        /*
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Semaphore semaphore = new Semaphore(3);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String name = Thread.currentThread().getName();
+                    System.out.println(name + " started working.");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        semaphore.acquire();
+                        workWithFileSystem();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        semaphore.release();
+                    }
+                    System.out.println(name + " finished working.");
+                }
+            });
+        }
+        executorService.shutdown();
+        */
+
+
+        //Task_12
+        /*
+        //List<Integer> numbers = Collections.synchronizedList(new ArrayList<>());
+        List<Integer> numbers = new CopyOnWriteArrayList<>();
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        Thread.sleep(100);
+                            numbers.add(i);
+                    }
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        Thread.sleep(100);
+                            numbers.add(i);
+                    }
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(numbers.size());
+        */
+
+
+        //Task_11
+        /*
+        MFP mfp = new MFP();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfp.scan(5);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfp.print(4);
+            }
+        }).start();
+        */
+
+
+        //Task_10
+        /*
+        Account account = new Account(1000, 1000);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                account.transferFrom1To2(300);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                account.transferFrom2To1(500);
+            }
+        }).start();
+        */
+
+
+        //Task_9
+        /*
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (MONITOR) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (!nextLetter.equals(A)) {
+                                MONITOR.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.print(A);
+                        nextLetter = B;
+                        MONITOR.notifyAll();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (MONITOR) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (!nextLetter.equals(B)) {
+                                MONITOR.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.print(B);
+                        nextLetter = C;
+                        MONITOR.notifyAll();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (MONITOR) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (!nextLetter.equals(C)) {
+                                MONITOR.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.print(C);
+                        nextLetter = A;
+                        MONITOR.notifyAll();
+                    }
+                }
+            }
+        }).start();
+        */
+
+        //Task_8
+        /*
+        //BlockingQueue blockingQueue = new BlockingQueue();
+        BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (true) {
+                    System.out.println("Counter: " + i);
+                    i++;
+                    Runnable task = null;
+                    try {
+                        task = blockingQueue.take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    new Thread(task).start();
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            blockingQueue.add(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("---" + index);
+                }
+            });
+        }
+        */
+
+
+        //Task_7
+        /*
+        ExecutorService executorService = Executors.newFixedThreadPool(3, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        System.out.print(".");
+                        Thread.sleep(300);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Future<String> futureName = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(5000);
+                return "John";
+            }
+        });
+
+        Future<Integer> futureAge = executorService.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                Thread.sleep(4000);
+                return 40;
+            }
+        });
+
+        try {
+            String name = futureName.get();
+            int age = futureAge.get();
+            System.out.println("\nName: " + name + "; Age: " + age);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        */
+
+
+        //Task_6
+        /*
+        Long a = 111L;
+        Long b = 111L;
+        Long c = 222L;
+        Long d = 222L;
+        System.out.println(a == b);
+        System.out.println(c == d);
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Runnable task1 = new Runnable() {
+            @Override
+            public void run() {
+                long sum = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 2 == 0) {
+                        sum += i;
+                    }
+                }
+                System.out.println("Task1: " + sum);
+                countDownLatch.countDown();
+            }
+        };
+        Runnable task2 = new Runnable() {
+            @Override
+            public void run() {
+                long sum = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 7 == 0) {
+                        sum += i;
+                    }
+                }
+                System.out.println("Task2: " + sum);
+                countDownLatch.countDown();
+            }
+        };
+        Runnable task3 = new Runnable() {
+            @Override
+            public void run() {
+                List<Integer> numbers = new ArrayList<>();
+                for (int i = 0; i < 1000; i++) {
+                    numbers.add((int) (Math.random() * 1000));
+                }
+                int count = 0;
+                for (int number : numbers) {
+                    if (number % 2 == 0) {
+                        count++;
+                    }
+                }
+                System.out.println("Task3: " + count);
+                countDownLatch.countDown();
+            }
+        };
+        long before = System.currentTimeMillis();
+        executorService.execute(task1);
+        executorService.execute(task2);
+        executorService.execute(task3);
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+        */
+
+
+        //Task_5
+        /*
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         CountDownLatch countDownLatch = new CountDownLatch(10);
         for (int i = 0; i < 10; i++) {
@@ -36,9 +401,10 @@ public class Main {
             throw new RuntimeException(e);
         }
         System.out.println("All threads are terminated");
+        */
 
 
-
+        //Task_4
         /*
         Counter2 counter2 = new Counter2();
         int barrier = 1_000_000;
@@ -70,6 +436,8 @@ public class Main {
         System.out.println(counter2.getValue());
         */
 
+
+        //Task_3
         /*
         ATM atm = new ATM(1000);
 
@@ -99,6 +467,8 @@ public class Main {
         thread3.start();
         */
 
+
+        //Task_2
         /*
         Counter counter = new Counter();
         long before = System.currentTimeMillis();
@@ -154,6 +524,8 @@ public class Main {
         System.out.println(after - before);
     */
 
+
+        //Task_1
     /*
     startTimer();
     withConcurrency();
@@ -235,5 +607,7 @@ public class Main {
         System.out.println("TimeWithConcurrency = " + (after - before));
     }
     */
+
+
     }
 }
